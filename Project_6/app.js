714bd3236dfe62e406fbf4790cba5212223ea785
pemalhanang <55036590+pemalhanang@users.noreply.github.com>
2020-06-28 11:29:46 -0500

    6/28 js upload

--- Project_6/app.js
+++ Project_6/app.js
@@ -1,9 +1,29 @@
+// create variables
 let qwerty = document.getElementById('qwerty');
 let phrase = document.getElementById('phrase');
-let startGame = document.getElementsByClassName('btn_reset');
+let startGame = document.querySelector('.btn__reset');
 let missed = 0;
+
+// create an array named phrases
 let phrases = [ 'f', 'b', 'e', 'g', 'c']
 
+//event listener to the start Game button
 startGame.addEventListener('click', () => {
   startGame.style.display = 'none';
 });
+
+// create a getRandomPhraseAsArray function
+function getRandomPhraseAsArray() {
+//random number based on length of array
+  let randomNum = phrases.length;
+  getRandomPhraseAsArray(phrases);
+}
+
+//create a checkLetter function
+function checkLetter(button){
+ let checkletter = document.getElementsByTagName('li');
+ let match;
+ for (i=0; i<checkLetter.length; i +=1) {
+    
+ }
+}
